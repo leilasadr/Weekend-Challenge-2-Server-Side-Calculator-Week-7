4fed21cb2f51c67e7f29fd634199fe47b943c653
Leila Sadr <leilasadr54@gmail.com>
2023-04-09 16:19:09 -0700

    imported the function(s) in the server.js

--- Server/Modules/calculator_data.js
+++ Server/Modules/calculator_data.js
@@ -1,38 +1,63 @@
 let calculationHistory = {
-    num1: 0,
-    num2: 0,
-    operation: '',
-    resNum: 0,
+    num1: [],
+    operation: [],
+    num2: [],
+    resNum: [],
 }
+
+let resNum = 0;
 
 function getCalculationHistory () {
     const copyOfcalculationHistory = calculationHistory;
     return copyOfcalculationHistory;
 }
 
-function calculate(operation, num1, num2) {
+function calculate(num1, operation, num2) {
     switch (operation) {
         case '+':
-            calculationHistory.resNum = num1 + num2 ;
+            resNum = num1 + num2 ;
             break;
         case '-':
-            calculationHistory.resNum = num1 - num2 ;
+            resNum = num1 - num2 ;
             break;
         case 'x':
-            calculationHistory.resNum = num1 * num2 ;
+            resNum = num1 * num2 ;
             break;
         case '/':
-            calculationHistory.resNum = num1 / num2 ;
+            resNum = num1 / num2 ;
             break;
     }
+    
+    // Creating a new calculation object with the details
+    let newCalculation = {
+        num1: num1,
+        operation: operation,
+        num2: num2,
+        resNum: resNum
+    };
+
+    // Pushing the new calculation object into the corresponding arrays
+    calculationHistory.num1.push(num1);
+    calculationHistory.num2.push(num2);
+    calculationHistory.operation.push(operation);
+    calculationHistory.resNum.push(resNum);
+
+    return resNum;
+
 }
 
 function resetCalculationHistory () {
     calculationHistory = {
         num1: [],
         num2: [],
-        operation: '',
+        operation: [],
         resNum: [],
     }
 }
+
+exports.getCalculationHistory = getCalculationHistory;
+exports.calculate = calculate;
+exports.resetCalculationHistory = resetCalculationHistory;
+
+
 
--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,4 @@
-
+let calculationS = require('./Modules/calculator_data.js');
 
 const express = require('express');
 const app = express();
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+let numberToCalculate = calculationS.calculate();
+console.log('numberToCalculate:', numberToCalculate);
 
 app.listen(5000, function() {
     console.log('you started the server! it is running on port 5000.');
