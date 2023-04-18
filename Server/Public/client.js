2099ebd201a87ba301c1605b702e8548a5c24d2b
Leila Sadr <leilasadr54@gmail.com>
2023-04-17 21:40:23 -0700

    got the GET route to work

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -1,12 +1,8 @@
 $(document).ready(onReady);
 
 function onReady () {
-  // $('#plus-button').on('click', handleButtonClick);
-  // $('#minus-button').on('click', handleButtonClick);
-  // $('#times-button').on('click', handleButtonClick);
-  // $('#divide-button').on('click', handleButtonClick);
   $('#calculatorForm').on('click', '.opperation', updateCurrentCalculation);
-  $('#equalsButton').on('click', sendToServer);
+  // $('#equalsButton').on('click', sendToServer);
 
   getCalculationHistory()
 }
@@ -17,59 +13,46 @@ console.log('starting currentCalculation:', currentCalculation);
     event.preventDefault();
     currentCalculation = $(this).text();
     console.log('updated currentCalculation:', currentCalculation);
-
-    // calculate(operation);
   }
 
-  // function handleClearButtonClick() {
-  //   resetCalculationHistory(handleCalculationHistoryReset);
-  //   $('#output-field').empty();
-  // }
+  // Updating the DOM with this GET route
+  function getCalculationHistory() {
 
-  // function handleCalculationHistoryReset() {
-  //   getCalculationHistory();
-  // }
+    $.ajax({
+      method: 'GET',
+      url: '/calculations',
+    }).then(
+      function (response) {
+        console.log('GET /calculations response:', response);
   
-  // function getCalculationHistory() {
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: '/calculations'
-  //   }).then(displayCalculationHistory)
-  // }
+        updateDomWithEquationHistory(response);
+      }
+    ).catch(
+      function (error) {
+        console.log('GET /calculations error:', error);
+      }
+    )
+  }
 
-  // function displayCalculationHistory(calculationHistory) {
-  //   // clearing the list
-  //   $('#calculation-history').empty();
-  //   // looping through each calculation and add a list item to the list
-  //   for (let i = 0; i < calculationHistory.num1.length; i++) {
-  //     let num1 = calculationHistory.num1[i];
-  //     let num2 = calculationHistory.num2[i];
-  //     let operation = calculationHistory.operation[i];
-  //     let resNum = calculationHistory.resNum[i];
-  //     $('#calculation-history').append(`
-  //       <li>
-  //         ${num1} ${operation} ${num2} = ${resNum}
-  //       </li>
-  //     `);
-  //   }
-  // }
- 
-  // function calculate(operation) {
-  //   let num1 = $('#num-1-input').val();
-  //   let num2 = $('#num-2-input').val();
-  //   $.ajax({
-  //     method: 'POST',
-  //     url: '/calculations',
-  //     data: {
-  //       num1: num1,
-  //       num2: num2,
-  //       operation: operation,
-  //       resNum: resNum
-  //     }
-  //   }).then(function(response) {
-  //     // displaying the result in the output field
-  //     $('#output-field').text(response.resNum);
-  //     // updating the calculation history
-  //     getCalculationHistory();
-  //   })
-  // }
+  function updateDomWithEquationHistory(equations) {
+    // equations is an array of equation objects.
+  
+    let newestAnswer = equations[equations.length - 1].answer;
+    $('#newestAnswer').text(`${newestAnswer}`);
+  
+    $('#calculationHistory').empty();
+  
+    for (let equation of equations) {
+  
+      let equationString = `
+      ${equation.numberOne} 
+      ${equation.opperation} 
+      ${equation.numberTwo} = ${equation.answer}`;
+
+      console.log(equationString);
+      $('#equation-history').append(`
+      <li class="list-item">
+        ${equationString}
+      </li>`);
+    }
+  }
--- Server/server.js
+++ Server/server.js
@@ -6,27 +6,23 @@ const bodyParser = require('body-parser');
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-let numberToCalculate = equations.calculate();
-console.log('numberToCalculate:', numberToCalculate);
-
 app.get('/calculations', (req, res) => {
-    let calculationHistory = calculationS.getCalculationHistory();
-    console.log('GET/calculations');
-    res.send(calculationHistory);
+  console.log('GET /calculations request received!');
+  res.send(equations);
 })
 
-app.post('/calculations', (req, res) => {
-    let num1 = Number(req.body.num1);
-    let num2 = Number(req.body.num2);
-    let operation = req.body.operation;
-    let resNum = calculate(num1, num2, operation);
+// app.post('/calculations', (req, res) => {
+//     let num1 = Number(req.body.num1);
+//     let num2 = Number(req.body.num2);
+//     let operation = req.body.operation;
+//     let resNum = calculate(num1, num2, operation);
     
-    // adding the calculation to the history
-    calculationS.addCalculation(num1, num2, operation, resNum);
+//     // adding the calculation to the history
+//     calculationS.addCalculation(num1, num2, operation, resNum);
     
-    console.log(`POST /calculations - ${num1} ${operation} ${num2} = ${resNum}`);
-    res.send({ resNum: resNum });
-  });
+//     console.log(`POST /calculations - ${num1} ${operation} ${num2} = ${resNum}`);
+//     res.send({ resNum: resNum });
+//   });
 
 app.listen(5000, function() {
     console.log('you started the server! it is running on port 5000.');
